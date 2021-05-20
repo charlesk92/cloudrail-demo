b01aff06f640c8c9a736449d7ff646eab06f171b
cysk92 <charles@indeni.com>
2021-05-20 08:52:43 -0700

    Add network infra

---
+++ network-infra/main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "nondefault" {
+  cidr_block = "10.1.1.0/24"
+  enable_dns_hostnames = true
+}
+
+resource "aws_security_group" "nondefault" {
+  vpc_id = aws_vpc.nondefault.id
+
+  ingress {
+    from_port = 0
+    protocol = "TCP"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_subnet" "nondefault_1" {
+  vpc_id = aws_vpc.nondefault.id
+  cidr_block = "10.1.1.128/25"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "nondefault_2" {
+  vpc_id = aws_vpc.nondefault.id
+  cidr_block = "10.1.1.0/25"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.nondefault.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.nondefault.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rt1" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id = aws_subnet.nondefault_1.id
+}
+
+resource "aws_route_table_association" "rt2" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id = aws_subnet.nondefault_2.id
+}
+
+resource "aws_db_subnet_group" nondefault {
+  name = "nondefault"
+  subnet_ids = [aws_subnet.nondefault_1.id, aws_subnet.nondefault_2.id]
+
+}
